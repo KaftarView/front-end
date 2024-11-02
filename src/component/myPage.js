18f7a7869945b7a90d114a42f05bdf21bb39c3ce
NazaninMahm <166870612+NazaninMahm@users.noreply.github.com>
2024-11-02 20:06:30 +0330

    feat: add boilerplate (#1)
    Co-authored-by: nazanin mahmoodi <nazaninm4646@gmail.com>

---
+++ src/component/myPage.js
@@ -0,0 +1,18 @@
+import './myPage.css';
+
+const Mypage = () => {
+    return ( 
+        <div>
+            <div >
+            <p className='fontfamilytest1' >لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</p>
+            </div> 
+            <div>
+                <p className='fontfamilytest2'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</p>
+            </div>
+
+        </div>
+
+ );
+}
+ 
+export default Mypage;
