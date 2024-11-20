2084b63dcada373a56a096ab2eef3e63872ee893
NazaninMahm <166870612+NazaninMahm@users.noreply.github.com>
2024-11-20 14:32:18 +0330

    refactor: refactoring boilerplate (#3)
    Co-authored-by: nazanin mahmoodi <nazaninm4646@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,77 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-// // 
-// let name : string ="Nazanin"
-// let age: number= 28
-// let isMariged : boolean= true
-// // 
+// // import { useState } from "react";
+// // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// // import "./App.css";
+// // import Login from "./components/Login";
+// // import SignUp from "./components/signup";
+
+// // function App() {
+// //   const [count, setCount] = useState(0);
+
+// //   return (
+// //     <div>
+// //       {/* <Login /> */}
+// //       <SignUp/>
+// //     </div>
+// //   );
+// // }
+
+// // export default App;
+
+
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import "./App.css";
+// import Login from "./components/Login";
+// import SignUp from "./components/signup";
+// import ForgetPassword from "./components/ForgetPassword";
+// import PrivateRoute from './components/PrivateRoute';  
+// import { AuthProvider, useAuth } from './components/AuthContext'; 
+// import Otp from './components/Otp'
+// import ResetPassword from './components/ResetPassword';
 
 // function App() {
-//   const [count, setCount] = useState(0)
-
+  
 //   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
+//     <Router>
+//       <Routes>
+//         <Route path="/signup" element={<SignUp />} />
+//         <Route path="/login" element={<Login />} />
+       
+//       </Routes>
+//     </Router>
+//   );
 // }
 
-// export default App
+// export default App;
 
 
-// import React from 'react'
-import Header from './components/Header'
-import  Router  from './router/Router'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Router from './router/Router';
+// import React from 'react';
+// import SignUp from './components/signup';
+// import Login from './components/Login';
+// import ForgetPassword from './components/ForgetPass/ForgetPassword';
+// import Otp from './components/Otp';
+// import ResetPassword from './components/ResetPassword';
+// import PrivateRoute from './components/PrivateRoute';  
+// import { AuthProvider, useAuth } from './components/AuthContext';
 
-const App = () => {
+function App() {
   return (
-    <div className='bg-gray-400'> 
-    <Header/>
-      <Router />
+    // <Router>
+    //   <Routes>
+        // <Route path="/signup" element={<SignUp />} />
+        // <Route path="/login" element={<Login />} />
+    //   </Routes>
+    // </Router>
+    <div className='my-app'>
+      <Router/>
     </div>
-
-  )
+  );
 }
 
-export default App
+
+
+export default App;
